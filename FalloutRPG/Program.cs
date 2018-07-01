5a9435379198dc92f05c47e43b1e25de01967268
MickMelon <michael.mcmillan@outlook.com>
2018-07-01 16:13:16 +0100

    Add project files.

---
+++ FalloutRPG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FalloutRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
