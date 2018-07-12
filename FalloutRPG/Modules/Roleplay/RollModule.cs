057c21cb354b688410d4e88a6ed3470374b19df6
MickMelon <michael.mcmillan@outlook.com>
2018-07-12 01:27:52 +0100

    Add mentions to roll messages

--- FalloutRPG/Modules/Roleplay/RollModule.cs
+++ FalloutRPG/Modules/Roleplay/RollModule.cs
@@ -31,75 +31,75 @@ namespace FalloutRPG.Modules.Roleplay
         #region SPECIAL Commands
         [Command("strength")]
         [Alias("str")]
-        public async Task RollStrength() => await ReplyAsync(await _rollService.GetSpRollAsync(Context.User, "Strength"));
+        public async Task RollStrength() => await ReplyAsync($"{await _rollService.GetSpRollAsync(Context.User, "Strength")} ({Context.User.Mention})");
 
         [Command("perception")]
         [Alias("per")]
-        public async Task RollPerception() => await ReplyAsync(await _rollService.GetSpRollAsync(Context.User, "Perception")); 
+        public async Task RollPerception() => await ReplyAsync($"{await _rollService.GetSpRollAsync(Context.User, "Perception")} ({Context.User.Mention})");
 
         [Command("endurance")]
         [Alias("end")]
-        public async Task RollEndurance() => await ReplyAsync(await _rollService.GetSpRollAsync(Context.User, "Endurance")); 
+        public async Task RollEndurance() => await ReplyAsync($"{await _rollService.GetSpRollAsync(Context.User, "Endurance")} ({Context.User.Mention})");
 
         [Command("charisma")]
         [Alias("cha")]
-        public async Task RollCharisma() => await ReplyAsync(await _rollService.GetSpRollAsync(Context.User, "Charisma")); 
+        public async Task RollCharisma() => await ReplyAsync($"{await _rollService.GetSpRollAsync(Context.User, "Charisma")} ({Context.User.Mention})");
 
         [Command("intelligence")]
         [Alias("int")]
-        public async Task RollIntelligence() => await ReplyAsync(await _rollService.GetSpRollAsync(Context.User, "Intelligence")); 
+        public async Task RollIntelligence() => await ReplyAsync($"{await _rollService.GetSpRollAsync(Context.User, "Intelligence")} ({Context.User.Mention})");
 
         [Command("agility")]
         [Alias("agi")]
-        public async Task RollAgility() => await ReplyAsync(await _rollService.GetSpRollAsync(Context.User, "Agility")); 
+        public async Task RollAgility() => await ReplyAsync($"{await _rollService.GetSpRollAsync(Context.User, "Agility")} ({Context.User.Mention})");
 
         [Command("luck")]
         [Alias("luc", "lck")]
-        public async Task RollLuck() => await ReplyAsync(await _rollService.GetSpRollAsync(Context.User, "Luck")); 
+        public async Task RollLuck() => await ReplyAsync($"{await _rollService.GetSpRollAsync(Context.User, "Luck")} ({Context.User.Mention})");
         #endregion
 
         #region Skills Commands
         [Command("barter")]
-        public async Task RollBarter() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "Barter")); 
+        public async Task RollBarter() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "Barter")} ({Context.User.Mention})"); 
 
         [Command("energy weapons")]
         [Alias("energy weapon", "energyweapons", "energyweapon", "energy")]
-        public async Task RollEnergyWeapons() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "EnergyWeapons")); 
+        public async Task RollEnergyWeapons() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "EnergyWeapons")} ({Context.User.Mention})");
 
         [Command("explosives")]
-        public async Task RollExplosives() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "Explosives")); 
+        public async Task RollExplosives() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "Explosives")} ({Context.User.Mention})");
 
         [Command("guns")]
-        public async Task RollGuns() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "Guns")); 
+        public async Task RollGuns() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "Guns")} ({Context.User.Mention})");
 
         [Command("lockpick")]
-        public async Task RollLockpick() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "Lockpick")); 
+        public async Task RollLockpick() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "Lockpick")} ({Context.User.Mention})");
 
         [Command("medicine")]
         [Alias("medic", "doctor")]
-        public async Task RollMedicine() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "Medicine")); 
+        public async Task RollMedicine() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "Medicine")} ({Context.User.Mention})");
 
         [Command("meleeweapons")]
         [Alias("melee", "meleeweapon", "melee weapons", "melee weapons")]
-        public async Task RollMeleeWeapons() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "MeleeWeapons")); 
+        public async Task RollMeleeWeapons() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "MeleeWeapons")} ({Context.User.Mention})");
 
         [Command("repair")]
-        public async Task RollRepair() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "Repair")); 
+        public async Task RollRepair() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "Repair")} ({Context.User.Mention})");
 
         [Command("science")]
-        public async Task RollScience() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "Science")); 
+        public async Task RollScience() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "Science")} ({Context.User.Mention})");
 
         [Command("sneak")]
-        public async Task RollSneak() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "Sneak"));
+        public async Task RollSneak() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "Sneak")} ({Context.User.Mention})");
 
         [Command("speech")]
-        public async Task RollSpeech() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "Speech"));
+        public async Task RollSpeech() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "Speech")} ({Context.User.Mention})");
 
         [Command("survival")]
-        public async Task RollSurvival() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "Survival"));
+        public async Task RollSurvival() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "Survival")} ({Context.User.Mention})");
 
         [Command("unarmed")]
-        public async Task RollUnarmed() => await ReplyAsync(await _rollService.GetSkillRollAsync(Context.User, "Unarmed"));
+        public async Task RollUnarmed() => await ReplyAsync($"{await _rollService.GetSkillRollAsync(Context.User, "Unarmed")} ({Context.User.Mention})");
         #endregion
     }
 }
